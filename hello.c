6a5e6aecfa0a0590d88b9b4e204d3727da31f973
rahul <rahulrajesh711@gmail.com>
2020-05-18 20:34:03 +0530

    Learned ignoring .vs files

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("hello world\n");
+    printf("Enter a Number\n");
+    scanf("%d",&a);
+    int b=a*a*a;
+    printf("Ans is %d",b);
+    return 0; 
+}
---
+++ leapyear.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter a Year: ");
+    scanf("%d",&a);
+}
